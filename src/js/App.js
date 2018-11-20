b1a9f63613b7bb535f6e2698c467d8e6ee7893a3
mathiasestdejapris <mmiraglio@edenschool.fr>
2018-11-20 15:43:00 +0100

    conflit

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
-import { Navbar, Jumbotron, Grid, Col } from 'react-bootstrap';
+import { Navbar, Jumbotron, Grid, Col, Image } from 'react-bootstrap';
+import Section from './Section';
 
 const App = () => (
     <Fragment>
@@ -21,22 +22,17 @@ const App = () => (
                 <h1 className="abelone text-center">Kanban</h1>
             </Jumbotron>
 
-             {/* Créer composant réutilisable */}
-            <section className="well">
-                <Grid fluid>
-                    <Col md={8}>
-                        <h2>Titre h2</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad voluptatem praesentium. Perspiciatis, dolores nulla repellat magni, repellendus rerum consequuntur nobis rem labore, tempore aperiam neque illo sapiente eligendi aspernatur!
-                        </p>
-                    </Col>
-                    <Col md={4}>
-                        <img alt="" src="https://placehold.it/512x512" />
-                    </Col>
-                </Grid>
+            <Section title="Titre H2" imageAlt="" imageSrc="https://placehold.it/512x512">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad voluptatem praesentium. Perspiciatis, dolores nulla repellat magni, repellendus rerum consequuntur nobis rem labore, tempore aperiam neque illo sapiente eligendi aspernatur!
+            </Section>
 
+            <Section title="Titre H2" imageAlt="" imageSrc="https://placehold.it/512x512">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad voluptatem praesentium. Perspiciatis, dolores nulla repellat magni, repellendus rerum consequuntur nobis rem labore, tempore aperiam neque illo sapiente eligendi aspernatur!
+            </Section>
 
-            </section>
+            <Section title="Titre H2" imageAlt="" imageSrc="https://placehold.it/512x512">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad voluptatem praesentium. Perspiciatis, dolores nulla repellat magni, repellendus rerum consequuntur nobis rem labore, tempore aperiam neque illo sapiente eligendi aspernatur!
+            </Section>
         </Grid>
     </Fragment>
 
