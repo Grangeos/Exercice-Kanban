1663ba2aa7d25e4ed7450e21e116213dca25608d
Adrien-OLR <aolivier@edenschool.fr>
2018-11-20 17:19:29 +0100

    Contenu Principe

--- src/js/App.js
+++ src/js/App.js
@@ -2,7 +2,9 @@ import React, { PureComponent, Fragment } from 'react';
 import { Navbar, Jumbotron, Grid, Col, Image } from 'react-bootstrap';
 import Section from './Section';
 import Header from "./Header";
+import Footer from "./Footer";
 import Introduction from "./Introduction";
+import Principes from "./Principe";
 
 
 class App extends PureComponent {
@@ -17,6 +19,7 @@ class App extends PureComponent {
               </Jumbotron>
 
               <Introduction/>
+              <Principes/>
 
             <Section title="Titre H2" imageAlt="" imageSrc="https://placehold.it/512x512">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad voluptatem praesentium. Perspiciatis, dolores nulla repellat magni, repellendus rerum consequuntur nobis rem labore, tempore aperiam neque illo sapiente eligendi aspernatur!
@@ -26,6 +29,7 @@ class App extends PureComponent {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad voluptatem praesentium. Perspiciatis, dolores nulla repellat magni, repellendus rerum consequuntur nobis rem labore, tempore aperiam neque illo sapiente eligendi aspernatur!
             </Section>
         </Grid>
+        <Footer/>
       </Fragment>
     );
   }
