c7ed88812c668ad3d6e31e96632b4aaed3ca9842
IKyllian <35967130+IKyllian@users.noreply.github.com>
2018-11-20 17:47:37 +0100

    Slider

--- src/js/Introduction.jsx
+++ src/js/Introduction.jsx
@@ -1,11 +1,11 @@
 import React, { PureComponent, Fragment } from 'react';
 import Section from "./Section";
-import imgIntroduction from "../Img/ImgFonctionnement.png";
+import ImgKanban from "../Img/Kanban.jpg";
 
 class Introduction extends PureComponent {
   render() {
     return(
-      <Section title="Introduction à Kanban" imageAlt="" imageSrc={imgIntroduction}>
+      <Section title="Introduction à Kanban" imageAlt="Image avec écrit Kanban sur un cahier" imageSrc={ImgKanban}>
         La méthodologie Kanban vient de l’industrie automobile au Japon, elle a été créée par Taiichi Ōno pour Toyota en 1950 dans le but d’optimiser leur capacité de production afin d’être compétitif face aux entreprises américaines.
         La méthode Kanban est basée sur l’approche “Lean”, qui est une amélioration continue des processus de production afin de permettre une gestion de la production sans gaspillage.
         La méthode “Lean” est fondée sur 4 principes :
