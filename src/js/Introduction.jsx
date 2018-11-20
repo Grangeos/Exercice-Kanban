a0a6ccf4a795342d85bd7ddf0ab805a1625e4902
IKyllian <35967130+IKyllian@users.noreply.github.com>
2018-11-20 16:52:38 +0100

    introduction

--- src/js/Introduction.jsx
+++ src/js/Introduction.jsx
@@ -9,10 +9,6 @@ class Introduction extends PureComponent {
         La méthodologie Kanban vient de l’industrie automobile au Japon, elle a été créée par Taiichi Ōno pour Toyota en 1950 dans le but d’optimiser leur capacité de production afin d’être compétitif face aux entreprises américaines.
         La méthode Kanban est basée sur l’approche “Lean”, qui est une amélioration continue des processus de production afin de permettre une gestion de la production sans gaspillage.
         La méthode “Lean” est fondée sur 4 principes :
-
-
-
-
         <ul>
           <li> Réduire les coûts de production </li>
           <li> Éviter la surproduction</li>
